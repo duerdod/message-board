6b63fc873e2e1e0377c4bb215fec0d47699629c0
Erik Pehrers <erik.pehrers@jetshop.se>
2019-07-20 20:26:02 +0200

    CRA, installed deps

---
+++ src/MessageBoard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function MessageBoard() {
+  return (
+    <div className="App">
+      <span />
+    </div>
+  );
+}
+
+export default MessageBoard;
