84f8804550f5a09f22897f2fe41f9958eba41c1c
Erik Pehrers <erik.pehrers@jetshop.se>
2019-08-07 10:18:54 +0200

    removed react-icons

--- client/src/Components/Forms/DesktopForm.js
+++ client/src/Components/Forms/DesktopForm.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { FormContext } from './Form';
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
 import { trimErrorMessage } from '../../utils/utils';
 import { ErrorMessage } from '../StatusPage';
 import { Title, Message, Name } from './FormInputs';
@@ -11,9 +11,7 @@ const DesktopForm = () => {
   const { step, availableInputs, error } = useContext(FormContext);
   return (
     <>
-      <StepButton direction="back">
-        <MdChevronLeft />
-      </StepButton>
+      <StepButton direction="back">></StepButton>
       {step.count === 0 && <Message />}
       {step.count === 1 && <Title />}
       {step.count === 2 && (
@@ -23,9 +21,7 @@ const DesktopForm = () => {
         </>
       )}
       {step.count !== availableInputs.length - 1 && (
-        <StepButton direction="forward">
-          <MdChevronRight />
-        </StepButton>
+        <StepButton direction="forward">></StepButton>
       )}
       {error ? (
         <ErrorMessage>{trimErrorMessage(error.message)}</ErrorMessage>
